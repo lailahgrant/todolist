116ee9b65fac5876cbb6fed3cb6b262768ea4901
lailahgrant <lailahgrant19@gmail.com>
2025-11-01 05:37:59 +0300

    Using State - stateful variables

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,32 @@
+import { useState } from 'react'
 import TodoInput from './components/TodoInput.jsx'
 import TodoList from './components/TodoList.jsx'
 
 function App() {
 
-  let todos = [
-    'Go to the gym',
+  /*  let todos = [
+      'Go to the gym',
+      'Learn React',
+      'Build a todo app'
+    ]
+    */
+
+  //state hook for todos - stateful variable
+  const [todos, setTodos] = useState([
+    /*'Go to the gym',
     'Learn React',
     'Build a todo app'
-  ]
+    */
+  ])
+
+  function handleAddTodos(newTodo) {
+    const newTodoList = [...todos, newTodo] //spread out old todod
+    setTodos(newTodoList)
+  }
 
   return (
     <>
-      <TodoInput />
+      <TodoInput handleAddTodos={handleAddTodos} />
       <TodoList todos={todos} />
     </>
   )
