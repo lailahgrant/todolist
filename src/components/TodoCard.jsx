7e90b1c307c1ac2122f9fd6be2dad0d290fa9509
lailahgrant <lailahgrant19@gmail.com>
2025-11-01 07:18:39 +0300

    Delete functionality in Todo done

--- src/components/TodoCard.jsx
+++ src/components/TodoCard.jsx
@@ -9,7 +9,7 @@ export default function TodoCard(props) {
                 <div className='actionsContainer'>
 
                     <button><i className="fa-solid fa-pen-to-square"></i></button>
-                    <button onClick={(e) => {
+                    <button onClick={() => {
                         handleDeleteTodo(index)
                     }} ><i className="fa-solid fa-trash"></i></button>
                 </div>
