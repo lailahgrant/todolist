79d9ef71e5ea3ce280880a4632856a81b4a71c13
lailahgrant <lailahgrant19@gmail.com>
2025-11-01 04:36:27 +0300

    index.css code

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,12 @@
-
+import TodoInput from './components/TodoInput.jsx'
+import TodoList from './components/TodoList.jsx'
 
 function App() {
-
-
   return (
-    <>
-
-    </>
+    <main>
+      <TodoInput />
+      <TodoList />
+    </main>
   )
 }
 
